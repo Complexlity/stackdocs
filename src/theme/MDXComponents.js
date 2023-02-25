7fbc65fc22248913f5568af4fc214e359cfca197
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-02-25 16:54:51 +0100

    Move QuestButton to components folder

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,6 +1,6 @@
 import React from "react";
 import MDXComponents from "@theme-original/MDXComponents";
-import QuestButton from "../../docs/QuestButton";
+import QuestButton from "../components/QuestButton";
 
 export default {
   ...MDXComponents,
