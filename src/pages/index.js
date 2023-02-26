a6899e40f203b238c1d72784030d7e8c2c650e1f
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-02-26 20:40:56 +0100

    Update hero section style

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link className="button button--secondary button--lg" to="/docs/">
-            Docusaurus Tutorial - 5min ⏱️
+            Start Linking 🔗
           </Link>
         </div>
       </div>
