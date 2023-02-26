d907457b97880af99c79398f786330e84485a8f0
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-02-26 21:15:00 +0100

    Update svgs on index page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,28 +4,29 @@ import styles from "./styles.module.css";
 
 const FeatureList = [
   {
-    title: "Easy to Use",
-    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+    title: "For Stackies",
+    Svg: require("@site/static/img/for-stackies.svg").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        StackDocs was designed from the ground up to help bring stackies closer
+        to the platform.
       </>
     ),
   },
   {
-    title: "Focus on What Matters",
-    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+    title: "Ease Of Access",
+    Svg: require("@site/static/img/ease.svg").default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        StackDocs lets you access your preferred campaign or quest in a single
+        click 👆. You also have access to all past campaigns and quests you may
+        have missed
       </>
     ),
   },
   {
     title: "Powered by React",
-    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+    Svg: require("@site/static/img/search.svg").default,
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
