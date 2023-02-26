c28c0e4c6e1dd85f08bf9e67e14d56ae133edca7
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-02-26 13:18:42 +0100

    Add comments to component

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,4 +1,5 @@
-import React from "react";
+// This file make <QuestButton/> component to be used anywhere in the code
+// See the official documentation for more information. https://docusaurus.io/docs/next/markdown-features/react#mdx-component-scope
 import MDXComponents from "@theme-original/MDXComponents";
 import QuestButton from "../components/QuestButton";
 
