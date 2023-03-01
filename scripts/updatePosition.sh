9dcd0b84543f412d2539174461666996f467978e
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-03-01 21:47:22 +0100

    fix: revert script function

--- scripts/updatePosition.sh
+++ scripts/updatePosition.sh
@@ -1,7 +1,7 @@
 # This file update's the 'position' property in all _category_.json files
 
 for file in docs/**/_category_.json; do
-  new_pos=$(jq '.position - 1' "$file")
+  new_pos=$(jq '.position + 1' "$file")
   sed -i "s/\"position\":.*/\"position\": $new_pos,/" "$file"
 done
 
