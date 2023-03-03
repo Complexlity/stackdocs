91bfffebe8ad4cfa292716bfc9da224588fd5ccf
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-03-03 23:55:11 +0100

    docs: change to descriptive names

--- scripts/createCampaign.py
+++ scripts/createCampaign.py
@@ -2,6 +2,7 @@ import os
 import json
 import sys
 
+# This updates the position property of the _category_.json files so the new campaign would be added at the top of the sidebar
 for root, dirs, files in os.walk('docs'):
     for file in files:
         if file == '_category_.json':
@@ -15,22 +16,22 @@ for root, dirs, files in os.walk('docs'):
 
 
 # Get the name from the command line argument
-name = sys.argv[1]
+campaign_name = sys.argv[1]
 
-# Create the folder name
-folder_name = "-".join(name.lower().split())
+# Create the folder name from the full name of the campaign
+campaign_folder_name = "-".join(campaign_name.lower().split())
 
 # Create the directory and change to it
-os.makedirs(os.path.join("docs", folder_name))
-os.chdir(os.path.join("docs", folder_name))
+os.makedirs(os.path.join("docs", campaign_folder_name))
+os.chdir(os.path.join("docs", campaign_folder_name))
 
-# Create the _category_.json file
+# Create the _category_.json file for the new campaign folder
 category = {
-    "label": name,
+    "label": campaign_name,
     "position": 2,
     "link": {
         "type": "doc",
-        "id": f"{folder_name}/index"
+        "id": f"{campaign_folder_name}/index"
     }
 }
 with open("_category_.json", "w") as f:
@@ -38,32 +39,21 @@ with open("_category_.json", "w") as f:
 
 # Create the index.mdx file
 with open("index.mdx", "w") as f:
-    f.write(f"# {name}\n\n")
+    f.write(f"# {campaign_name}\n\n")
     f.write("_Brief Overview Goes Here_\n\n")
     f.write('import DocCardList from "@theme/DocCardList";\n\n')
     f.write("<DocCardList />\n\n")
     f.write(":::info FULL OVERVIEW ?\n\n")
     f.write('<QuestButton\n')
     f.write('  text="Go To Campaign Page"\n')
-    f.write(f'  link="/docs/{folder_name}/"\n')
+    f.write(f'  link="/docs/{campaign_folder_name}/"\n')
     f.write('/>\n\n')
     f.write(":::\n")
 
-# # Create the three .md files
-# for i in range(3):
-#     file_name = f"{sys.argv[i+2].lower().replace(' ', '-')}.md"
-#     with open(file_name, "w") as f:
-#         f.write(f"---\nsidebar_position: {i+1}\n---\n\n")
-#         f.write(f"# {sys.argv[i+2]}\n\n")
-#         f.write("_Brief Overview Goes Here_\n\n")
-#         f.write(":::tip Happy Learning!!\n\n")
-#         f.write('<QuestButton text="Go To Quest" link="" />\n\n')
-#         f.write(":::\n")
-
-# Create the .md files
+# Create the .md files for each quest 
 for i in range(2, len(sys.argv)):
-    file_name = f"{sys.argv[i].lower().replace(' ', '-')}.md"
-    with open(file_name, "w") as f:
+    quest_file_name = f"{sys.argv[i].lower().replace(' ', '-')}.md"
+    with open(quest_file_name, "w") as f:
         f.write(f"---\nsidebar_position: {i-1}\n---\n\n")
         f.write(f"# {sys.argv[i]}\n\n")
         f.write("_Brief Overview Goes Here_\n\n")
