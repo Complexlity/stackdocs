1c222168e6e678e803c6a4003e50e2470036fd86
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-05-14 00:08:17 +0100

    feat: add tracks categories

--- sidebarTracks.js
+++ sidebarTracks.js
@@ -1,5 +1,15 @@
 // This file is a work in progress
 
 module.exports = {
-  tracks: ["coming-soon"],
+  tracks: {
+    "Mega Campaigns": ["coming-soon"],
+    "Front-End Development": ["coming-soon"],
+    "Back-End Development": ["coming-soon"],
+    "Blockchain and Web3": ["coming-soon"],
+    "Data Science": ["coming-soon"],
+    CyberSecurity: ["coming-soon"],
+    "Machine Learning": ["coming-soon"],
+    "Pure Languages": ["coming-soon"],
+    Utilities: ["coming-soon"],
+  },
 };
